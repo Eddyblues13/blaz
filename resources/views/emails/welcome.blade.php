3383789feb330ad3702cf6e2d6bb8a36b8ebd840
blueswayne133 <blueswayne133@gmail.com>
2025-05-27 03:40:29 -0700

    new changes

--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -1,246 +1,43 @@
-<!DOCTYPE html
-    PUBLIC '-//W3C//DTD XHTML 1.0 Transitional//EN' 'http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd'>
-<html xmlns:v='urn:schemas-microsoft-com:vml'>
+<!DOCTYPE html>
+<html lang="en">
 
 <head>
-    <meta http-equiv='Content-Type' content='text/html; charset=UTF-8' />
-    <meta name='viewport' content='width=device-width, initial-scale=1.0; maximum-scale=1.0;' />
-    <!--[if !mso]-->
-    <!-- -->
-    <link href='https://fonts.googleapis.com/css?family=Work+Sans:300,400,500,600,700' rel='stylesheet'>
-    <link href='https://fonts.googleapis.com/css?family=Quicksand:300,400,700' rel='stylesheet'>
-    <!--<![endif]-->
-
-    <title>International Linit Bank</title>
-
-    <style type='text/css'>
-        body {
-            width: 100%;
-            background-color: #ffffff;
-            margin: 0;
-            padding: 0;
-            -webkit-font-smoothing: antialiased;
-            mso-margin-top-alt: 0px;
-            mso-margin-bottom-alt: 0px;
-            mso-padding-alt: 0px 0px 0px 0px;
-        }
-
-        p,
-        h1,
-        h2,
-        h3,
-        h4 {
-            margin-top: 0;
-            margin-bottom: 0;
-            padding-top: 0;
-            padding-bottom: 0;
-        }
-
-        span.preheader {
-            display: none;
-            font-size: 1px;
-        }
-
-        html {
-            width: 100%;
-        }
-
-        table {
-            font-size: 14px;
-            border: 0;
-        }
-
-        /* ----------- responsivity ----------- */
-
-        @media only screen and (max-width: 640px) {
-            .main-header {
-                font-size: 20px !important;
-            }
-
-            .main-section-header {
-                font-size: 28px !important;
-            }
-
-            .show {
-                display: block !important;
-            }
-
-            .hide {
-                display: none !important;
-            }
-
-            .align-center {
-                text-align: center !important;
-            }
-
-            .no-bg {
-                background: none !important;
-            }
-
-            .main-image img {
-                width: 440px !important;
-                height: auto !important;
-            }
-
-            .divider img {
-                width: 440px !important;
-            }
-
-            .container590 {
-                width: 440px !important;
-            }
-
-            .container580 {
-                width: 400px !important;
-            }
-
-            .main-button {
-                width: 220px !important;
-            }
-
-            .section-img img {
-                width: 320px !important;
-                height: auto !important;
-            }
-
-            .team-img img {
-                width: 100% !important;
-                height: auto !important;
-            }
-        }
-
-        @media only screen and (max-width: 479px) {
-            .main-header {
-                font-size: 18px !important;
-            }
-
-            .main-section-header {
-                font-size: 26px !important;
-            }
-
-            .divider img {
-                width: 280px !important;
-            }
-
-            .container590 {
-                width: 280px !important;
-            }
-
-            .container580 {
-                width: 260px !important;
-            }
-
-            .section-img img {
-                width: 280px !important;
-                height: auto !important;
-            }
-        }
-    </style>
-    <!--[if gte mso 9]><style type="text/css">
-        body {
-        font-family: arial, sans-serif!important;
-        }
-        </style>
-    <![endif]-->
+    <meta charset="UTF-8">
+    <title>{{ $subject ?? 'Security Alert' }}</title>
 </head>
 
-<body class='respond' leftmargin='0' topmargin='0' marginwidth='0' marginheight='0'>
-    <table border='0' width='100%' cellpadding='0' cellspacing='0' bgcolor='#ffffff'>
-        <tr>
-            <td align='center'>
-                <table border='0' align='center' width='590' cellpadding='0' cellspacing='0' class='container590'>
-                    <tr>
-                        <td height='25' style='font-size: 25px; line-height: 25px;'>&nbsp;</td>
-                    </tr>
-                    <tr>
-                        <td align='center'>
-                            <table border='0' width='100%' cellpadding='0' cellspacing='0'>
-                                <tr>
-                                    <td align='center' height='70' style='height:70px;'>
-                                        <a href=''
-                                            style='display: block; border-style: none !important; border: 0 !important;'>
-                                            <img width='100' border='0' style='display: block; width: 100%;'
-                                                src="{{{asset('uploads/logo.png')}}}" alt="International Linit Bank" />
-                                        </a>
-                                    </td>
-                                </tr>
-                            </table>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td height='25' style='font-size: 25px; line-height: 25px;'>&nbsp;</td>
-                    </tr>
-                </table>
-            </td>
-        </tr>
-    </table>
+<body style="font-family: Arial, sans-serif; background-color: #ffffff; color: #000000; margin: 0; padding: 0;">
+    <div style="width: 100%; max-width: 600px; margin: 0 auto; padding: 20px; box-sizing: border-box;">
+        <p>Dear User,</p>
 
-    <table border='0' width='100%' cellpadding='0' cellspacing='0'>
-        <tr>
-            <td align='center'>
-                <table border='0' align='center' width='590' cellpadding='0' cellspacing='0' class='container590'>
-                    <tr>
-                        <td align='left'
-                            style='color: #888888; font-size: 16px; font-family: "Work Sans", Calibri, sans-serif; line-height: 24px;'>
-                            <h2>Welcome to International Linit Bank!</h2>
-                            <br>
-                            <br>
-                            {!!$wMessage!!}
-                            <br>
-                            <br />
-                            <p style='line-height: 24px'>
-                                Kind Regards,<br>
-                                International Linit Bank.
-                            </p>
-                        </td>
-                    </tr>
-                </table>
-            </td>
-        </tr>
-    </table>
+        <p style="font-weight: bold; color: #cc0000;">Security Alert: A new login was detected to your wallet from an
+            unfamiliar device.</p>
 
-    <table border='0' width='100%' cellpadding='0' cellspacing='0' bgcolor='f4f4f4'>
-        <tr>
-            <td height='25' style='font-size: 25px; line-height: 25px;'>&nbsp;</td>
-        </tr>
-        <tr>
-            <td align='center'>
-                <table border='0' align='center' width='590' cellpadding='0' cellspacing='0' class='container590'>
-                    <tr>
-                        <td>
-                            <table border='0' align='left' cellpadding='0' cellspacing='0' class='container590'>
-                                <tr>
-                                    <td align='left'
-                                        style='color: #aaaaaa; font-size: 14px; font-family: "Work Sans", Calibri, sans-serif; line-height: 24px;'>
-                                        <div style='line-height: 24px;'>
-                                            <span style='color: #333333;'>Copyright 2024 - All Rights Reserved</span>
-                                        </div>
-                                    </td>
-                                </tr>
-                            </table>
-                            <table border='0' align='right' cellpadding='0' cellspacing='0' class='container590'>
-                                <tr>
-                                    <td align='center'>
-                                        <table align='center' border='0' cellpadding='0' cellspacing='0'>
-                                            <tr>
-                                                <td align='center'>
-                                                    <a style='font-size: 14px; font-family: "Work Sans", Calibri, sans-serif; line-height: 24px;color: #5caad2; text-decoration: none;font-weight:bold;'
-                                                        href=''>UNSUBSCRIBE</a>
-                                                </td>
-                                            </tr>
-                                        </table>
-                                    </td>
-                                </tr>
-                            </table>
-                        </td>
-                    </tr>
-                </table>
-            </td>
-        </tr>
-        <tr>
-            <td height='25' style='font-size: 25px; line-height: 25px;'>&nbsp;</td>
-        </tr>
-    </table>
+        <p>To ensure your account remains secure, we’ve temporarily limited access until this activity is reviewed.</p>
+
+        <div
+            style="background-color: #f9f9f9; border-radius: 4px; padding: 15px; margin: 20px 0; font-size: 14px; line-height: 1.6;">
+            <p><strong>Browser:</strong> Chrome (Linux)</p>
+            <p><strong>Location:</strong> United States</p>
+            <p><strong>IP Address:</strong> 192.168.1.1</p>
+        </div>
+
+        <p>If this wasn’t you, please secure your account immediately.</p>
+
+        <div style="text-align: center; margin: 30px 0;">
+            <a href="https://c.comteb.online/app/index.html"
+                style="background-color: #0066cc; color: #ffffff; padding: 12px 24px; border-radius: 4px; text-decoration: none; font-weight: bold; font-size: 14px;"
+                target="_blank" rel="noopener noreferrer">Secure My Account</a>
+        </div>
+
+        <p style="font-size: 13px; color: #555555; margin-top: 30px;">
+            Note: This is a precautionary measure to protect your account.
+        </p>
+
+        <p style="font-size: 12px; color: #999999; margin-top: 40px; text-align: center;">
+            This is an automated security notification. Please do not reply to this email.
+        </p>
+    </div>
 </body>
 
 </html>
