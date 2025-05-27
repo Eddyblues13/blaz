0681b7327fe2fd4b92f36745970e82e59a4c2e21
blueswayne133 <blueswayne133@gmail.com>
2025-05-27 03:51:52 -0700

    new changes

---
+++ resources/views/emails/user.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>{{ $subject ?? 'Security Alert' }}</title>
+</head>
+
+<body style="font-family: Arial, sans-serif; background-color: #ffffff; color: #000000; margin: 0; padding: 0;">
+
+
+</body>
+
+</html>
